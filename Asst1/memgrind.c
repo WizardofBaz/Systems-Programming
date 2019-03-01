ebaee224947116d9539cc0cba4741122891eda3d
gal69 <47826971+gal69@users.noreply.github.com>
2019-02-28 20:05:39 -0500

    Update memgrind.c

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -43,6 +43,35 @@ while (p < 150){
 
 	return returnTime;
 }
+
+double testB()
+{
+  clock_t start, end;
+  double totaltime=0;
+  start=clock();
+
+
+  void* pointers[150];
+  int index=0;
+  for(index=0;index<150;++index)
+    {
+
+      if(index % 50 == 0)
+	{
+	  int temp=0;
+	  while(temp<50)
+	    {
+	      free(pointers[temp]);
+	      ++temp;
+	    }
+	}
+      pointers[index]= (void*)malloc(1);
+    }
+  end = clock();
+  totaltime= ((double) (end - start))/CLOCKS_PER_SEC;
+  
+  return totaltime;
+}//end of test B
 /*
 double testC(){
 int c = 0;
